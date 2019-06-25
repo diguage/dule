4368b59212c999f0fb1f509978bb6e5fe7483f34
diguage <leejun119@gmail.com>
2019-06-25 11:56:20 +0800

    init project

---
+++ src/main/java/com/diguage/dule/WebUtil.java
@@ -0,0 +1,9 @@
+package com.diguage.dule;
+
+/**
+ * @author D瓜哥, https://www.diguage.com/
+ * @since 2019-06-25 11:53
+ */
+public class WebUtil {
+
+}
---
+++ src/test/java/com/diguage/dule/WebUtilTest.java
@@ -0,0 +1,8 @@
+package com.diguage.dule;
+
+/**
+ * @author D瓜哥, https://www.diguage.com/
+ * @since 2019-06-25 11:54
+ */
+public class WebUtilTest {
+}
